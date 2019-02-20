d4d1b93c2a2c7db2ac83e8cb89a4b3849ab71eff
MansourMoukdad88 <m_sy@outlook.com>
2019-02-21 01:41:20 +0200

    Fix Bug

--- server/index.js
+++ server/index.js
@@ -50,6 +50,13 @@ app.get("/repos", function(req, res) {
 });
 
 //let port = 1128;
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
