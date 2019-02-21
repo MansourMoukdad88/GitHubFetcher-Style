e1587491df7d1bd735ad713c1fe067f55cc48e37
MansourMoukdad88 <m_sy@outlook.com>
2019-02-21 11:19:44 +0200

    Push to Heroku

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/repos", function(req, res) {
   });
 });
 
-app.get("/repos", function(req, res) {
+app.get("/", function(req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
   sqlData.getRepos(req.body.username, function(data) {
